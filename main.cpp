3adc0881ed64840f723b67770046d6f1721bce47
natnat <nathan_mathew_carr@hotmail.co.uk>
2024-05-26 15:16:35 +0100

    file is now opening

--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <CascLib.h>
 
-int main() {
+int main()
+{
     HANDLE hStorage;
     HANDLE hFile;
     DWORD dwBytesRead;
     char buffer[1024];
-    const char *storagePath = "D:/Games/BattleNet/World of Warcraft/data";
+    const char *storagePath = "D:/Games/BattleNet/World of Warcraft*wow";
 
     // Open the storage
-    if (!CascOpenStorage(storagePath, CASC_LOCALE_ENUS, &hStorage)) {
+    if (!CascOpenStorage(storagePath, CASC_LOCALE_ENUS, &hStorage))
+    {
         std::cerr << "Failed to open storage" << std::endl;
         return 1;
     }
 
     // Open a file from the storage
-    if (!CascOpenFile(hStorage, "Interface\\FrameXML\\Localization.lua", 0, 0, &hFile)) {
-        std::cerr << "Failed to open file" << std::endl;
+    if (!CascOpenFile(hStorage, "Interface/FrameXML/Localization.lua", 0, 0, &hFile))
+    {
+        std::cerr << "Failed to open file, casc error:" << GetLastError() << std::endl;
         CascCloseStorage(hStorage);
         return 1;
     }
 
     // Read from the file
-    if (CascReadFile(hFile, buffer, sizeof(buffer) - 1, &dwBytesRead)) {
+    if (CascReadFile(hFile, buffer, sizeof(buffer) - 1, &dwBytesRead))
+    {
         buffer[dwBytesRead] = '\0';
         std::cout << "File content: " << buffer << std::endl;
-    } else {
+    }
+    else 
+    {
         std::cerr << "Failed to read file" << std::endl;
     }
 
