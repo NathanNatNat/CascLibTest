b8efbc27174ad052de40c7253e187422f63ccea6
natnat <nathan_mathew_carr@hotmail.co.uk>
2024-05-29 04:40:29 +0100

    adjusted includes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <glad/gl.h>
 // GLFW (include after glad)
 #include <GLFW/glfw3.h>
